37b9f640aeb9ef9bb88b3bd9c0ecded3cd486882
Derek Chan <derekchan1994@gmail.com>
2016-01-18 09:31:04 -0800

    Replaced serial port device addressing with macros

--- exercise1.3/software/minecraft_go/src/serial.c
+++ exercise1.3/software/minecraft_go/src/serial.c
@@ -11,7 +11,13 @@
 #define SERIAL_STATUS_MASK_TX_BIT	(0x02)
 #define SERIAL_STATUS_MASK_RX_BIT	(0x01)
 
-void serial_init(unsigned volatile char * DEV_CONTROL, unsigned volatile char * DEV_BAUD) {
+#define DEV_CONTROL	(DEV)
+#define DEV_STATUS	(DEV)
+#define DEV_TX_DATA	(DEV + 2)
+#define DEV_RX_DATA	(DEV + 2)
+#define DEV_BAUD	(DEV + 4)
+
+void serial_init(unsigned volatile char * DEV) {
 	/**
 	 * Set up 6850 Control Register to use
 	 * * Divide by 16 clock (CR1/CR0 = 01)
@@ -27,7 +33,7 @@ void serial_init(unsigned volatile char * DEV_CONTROL, unsigned volatile char * 
 	*DEV_BAUD = 0x01;
 }
 
-void serial_put_char(const unsigned volatile char * DEV_STATUS, unsigned volatile char * DEV_TX_DATA, const unsigned volatile char c) {
+void serial_put_char(const unsigned volatile char * DEV, const unsigned char c) {
 	/* Poll Tx bit in 6850 status register. Wait for it to become '1' */
 	while ((*DEV_STATUS & SERIAL_STATUS_MASK_TX_BIT) == 0)
 		/* Wait */;
@@ -36,7 +42,7 @@ void serial_put_char(const unsigned volatile char * DEV_STATUS, unsigned volatil
 	*DEV_TX_DATA = c;
 }
 
-unsigned char serial_get_char(const unsigned volatile char * DEV_STATUS, const unsigned volatile char * DEV_RX_DATA) {
+unsigned char serial_get_char(const unsigned volatile char * DEV) {
 	/* Poll Rx bit in 6850 status register. Wait for it to become '1' */
 	while ((*DEV_STATUS & SERIAL_STATUS_MASK_RX_BIT) == 0)
 		/* Wait */;
@@ -45,8 +51,10 @@ unsigned char serial_get_char(const unsigned volatile char * DEV_STATUS, const u
 	return (*DEV_RX_DATA);
 }
 
-int serial_test_for_received_data(const unsigned volatile char * DEV_STATUS) {
+int serial_test_for_received_data(const unsigned volatile char * DEV) {
 	/* Test Rx bit in 6850 serial comms chip status register */
 	/* If RX bit is set, return TRUE, otherwise return FALSE */
 	return ((*DEV_STATUS & SERIAL_STATUS_MASK_RX_BIT) ? 1 : 0);
 }
+
+int serial_put_n_char(const unsigned volatile char * DEV, const )
