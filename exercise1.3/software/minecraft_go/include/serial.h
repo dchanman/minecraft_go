35e4a48e9793ed671c5268413068df8a69f2c208
Derek Chan <derekchan1994@gmail.com>
2016-01-18 09:44:27 -0800

    Added put_n and get_n functions into the serial libary

--- exercise1.3/software/minecraft_go/include/serial.h
+++ exercise1.3/software/minecraft_go/include/serial.h
@@ -19,12 +19,30 @@ void serial_init(unsigned volatile char * DEV);
 /**
  * Writes a single character to the serial port
  */
-void serial_put_char(const unsigned volatile char * DEV, const unsigned char c);
+void serial_put_char(unsigned volatile char * DEV, const unsigned char c);
+
+/**
+ * Writes an array of characters to the serial port
+ *
+ * @param DEV - the address of the serial port device
+ * @param msg - the address of an array containing characters
+ * @param msg_length - the number of characters to write to the device
+ */
+void serial_put_n_char(unsigned volatile char * DEV, const unsigned char * msg, const int msg_length);
 
 /**
  * Reads a single character from the rs232 serial port
  */
 unsigned char serial_get_char(const unsigned volatile char * DEV);
+
+/**
+ * Reads a sequence of characters from the serial port.
+ *
+ * @param DEV - the address of the serial port device
+ * @param recv_msg - a buffer to store all received characters
+ * @param recv_msg_length - the number of characters to read into the buffer
+ */
+void serial_get_n_char(const unsigned volatile char * DEV, unsigned char * recv_msg, const int recv_msg_length);
 
 /**
  * Poll the serial port to determine if any character has been received.
