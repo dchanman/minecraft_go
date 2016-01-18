79a19ec7a5308aeccf9c69923b2d48d013516424
Derek Chan <derekchan1994@gmail.com>
2016-01-18 09:31:04 -0800

    Replaced serial port device addressing with macros

--- exercise1.3/software/minecraft_go/include/serial.h
+++ exercise1.3/software/minecraft_go/include/serial.h
@@ -14,23 +14,23 @@
  * Call this function at the start of the program prior to reading
  * or writing data via the Serial port.
  */
-void serial_init(unsigned volatile char * DEV_CONTROL, unsigned volatile char * DEV_BAUD);
+void serial_init(unsigned volatile char * DEV);
 
 /**
  * Writes a single character to the serial port
  */
-void serial_put_char(const unsigned volatile char * DEV_STATUS, unsigned volatile char * DEV_TX_DATA, const unsigned volatile char c);
+void serial_put_char(const unsigned volatile char * DEV, const unsigned char c);
 
 /**
  * Reads a single character from the rs232 serial port
  */
-unsigned char serial_get_char(const unsigned volatile char * DEV_STATUS, const unsigned volatile char * DEV_RX_DATA);
+unsigned char serial_get_char(const unsigned volatile char * DEV);
 
 /**
  * Poll the serial port to determine if any character has been received.
  * It doesn't wait for one, or read it. It simply tests to see
  * if there is one available to read.
  */
-int serial_test_for_received_data(const unsigned volatile char * DEV_STATUS);
+int serial_test_for_received_data(const unsigned volatile char * DEV);
 
 #endif /* SERIAL_H_ */
