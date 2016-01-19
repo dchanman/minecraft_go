67305bcb3bdc955a873e6230d850777c3dba2c82
Derek Chan <derekchan1994@gmail.com>
2016-01-18 21:15:23 -0800

    Added put_n and get_n functions to rs232 lib. Moved test code into test/ directory

---
+++ exercise1.3/software/minecraft_go/include/rs232_test.h
@@ -0,0 +1,15 @@
+/*
+ * rs232_test.h
+ *
+ *  Created on: Jan 18, 2016
+ *      Author: derek
+ */
+
+#ifndef RS232_TEST_H_
+#define RS232_TEST_H_
+
+int rs232_test_single_char();
+int rs232_test_multi_char();
+
+
+#endif /* RS232_TEST_H_ */
--- exercise1.3/software/minecraft_go/src/rs232.c
+++ exercise1.3/software/minecraft_go/src/rs232.c
@@ -19,8 +19,16 @@ void rs232_put_char(const unsigned char c) {
 	serial_put_char(RS232, c);
 }
 
+void rs232_put_n_char(const unsigned char * msg, const int msg_length) {
+	serial_put_n_char(RS232, msg, msg_length);
+}
+
 unsigned char rs232_get_char() {
 	return serial_get_char(RS232);
+}
+
+void rs232_get_n_char(unsigned char * recv_msg, const int recv_msg_length) {
+	return serial_get_n_char(RS232, recv_msg, recv_msg_length);
 }
 
 int rs232_test_for_received_data() {
