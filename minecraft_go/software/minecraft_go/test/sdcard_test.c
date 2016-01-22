08a4804c3c55962cf0824ac05754f48b18a53421
Derek Chan <derekchan1994@gmail.com>
2016-01-21 23:14:15 -0800

    Minimum files

---
+++ minecraft_go/software/minecraft_go/test/sdcard_test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <altera_up_sd_card_avalon_interface.h>
+
+void sdcard_test() {
+	short int filehandle;
+	const char * msg = "CPEN391 MINECRAFT GO\n";
+	const char * msg2 = "This is a test\n";
+
+	sdcard_init();
+	sdcard_open(&filehandle, "hello1.txt");
+	sdcard_write(&filehandle, msg, strlen(msg));
+	sdcard_write(&filehandle, msg2, strlen(msg2));
+	sdcard_close(&filehandle);
+}
