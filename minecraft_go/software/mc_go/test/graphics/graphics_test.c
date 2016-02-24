b99e7ef2b3cdd4d896efc607c0c0e47ae78b2454
Jay Dahiya <e1x8@ece.ubc.ca>
2016-02-23 17:33:04 -0800

    Crazy Demo Code

--- minecraft_go/software/mc_go/test/graphics/graphics_test.c
+++ minecraft_go/software/mc_go/test/graphics/graphics_test.c
@@ -7,18 +7,32 @@
 #include "ColourPallette.h"
 
 #define YRES 480
-#define XRES 640
+#define XRES 800
 #define TRUE 1
 #define FALSE 0
 
 
 int graphics_test() {
 	//ProgramPalette(BLUE,0x000000FF);
-	int i;
-	for (i = 0; i <= 480; i++) {
-		HLine(0, i, 640, WHITE);
-	}
 
+	int i = 0;
+	int x1,x2,y1,y2, color;
+
+	graphics_clear_screen();
+
+	//CRAZY DEMO CODE
+	/*
+	while(1) {
+		x1 = rand() % XRES;
+		y1 = rand() % YRES;
+		x2 = rand() % XRES;
+		y2 = rand() % YRES;
+
+		color = rand() % 100;
+		Line2(x1, y1, x2, y2, color);
+
+	}
+*/
 	Line2(0, 0, 400, 400, BLUE);
 	HLine(0, 100, 640, BLUE);
 	VLine2(110, 0, 450, BLUE);
@@ -27,12 +41,18 @@ int graphics_test() {
 	Line(0, 0, 400, 400, GREEN);
 	Line(640, 480, 400, 400, GREEN);
 
+
 	Triangle(110, 10, 100, 100, 150, 400, PINK);
 
-//FillRectangle(10,10,10,300,300,10, 300,300, GOLD);
+	graphics_draw_rectangle_filled(400, 200, 50, 10, GOLD);
+	graphics_draw_rectangle_border(400, 200, 50, 10, OLIVE);
 
 //Fonts bigger than 5x7 are a work in progress
-	OutGraphicsCharFont2a(300, 100, BLUE, ORANGE, 'G', FALSE);
+	OutGraphicsCharFont2a(300, 100, BLUE, ORANGE, 'A', FALSE);
+	OutGraphicsCharFont3(300, 300, BLUE, ORANGE, 'A', FALSE);
+
+	OutGraphicsCharFont4(300, 300, BLUE, ORANGE, 'A', FALSE);
+	OutGraphicsCharFont5(300, 100, BLUE, ORANGE, 'A', FALSE);
 
 	return 0;
 }
