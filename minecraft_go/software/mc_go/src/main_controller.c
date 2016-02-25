bb9a6d774e16c44bd4cbc2d0541d977b97d1e84d
Derek Chan <derekchan1994@gmail.com>
2016-02-25 12:00:40 -0800

    Refactored main_controller a little, ready for better refactoring later

--- minecraft_go/software/mc_go/src/main_controller.c
+++ minecraft_go/software/mc_go/src/main_controller.c
@@ -19,6 +19,8 @@
 #include "journey_display.h"
 #include "touchscreen.h"
 
+#define DISTANCE_TO_DESTINATION_THRESHOLD	5
+
 typedef enum {
 	STATE_JOURNEY = 0,
 	STATE_RECEIVE_COORDINATES,
@@ -32,13 +34,10 @@ static state_t main_controller_state_receive_coordinates(savedata_t *data);
 static state_t main_controller_state_creeper_minigame(savedata_t *data);
 static state_t main_controller_state_arrived_at_destination(savedata_t *data);
 static state_t main_controller_state_report_results(savedata_t *data);
+static state_t main_controller_journey_menu_buttons(const savedata_t data);
 
 void main_controller_run() {
 	savedata_t data;
-	char sw;
-	Time elapsed_time;
-	unsigned long elapsed_time_seconds;
-	boolean result;
 	state_t current_state = STATE_JOURNEY;
 	state_t prev_state = current_state;
 
@@ -90,7 +89,6 @@ void main_controller_run() {
  */
 static state_t main_controller_state_journey(savedata_t *data) {
 	Time current_time;
-	Location current_location;
 	double distance_to_destination;
 	float speed;
 
@@ -100,7 +98,7 @@ static state_t main_controller_state_journey(savedata_t *data) {
 	gps_convert_seconds_to_time(&current_time, journey_time);
 
 	/* Check if we've arrived at our destination */
-	if (distance_to_destination < 5)
+	if (distance_to_destination < DISTANCE_TO_DESTINATION_THRESHOLD)
 		return STATE_ARRIVED_AT_DESTINATION;
 
 	/* Update journey display */
@@ -108,34 +106,7 @@ static state_t main_controller_state_journey(savedata_t *data) {
 	displayMenu(&current_time, distance_to_destination, speed, data->creeps_defeated);
 
 	/* Poll for user input */
-	Pixel touch_location;
-	boolean touch_result;
-	touch_result = touchscreen_get_press(&touch_location, 800);
-
-	if (touch_result) {
-		// TODO refactor button interface
-		Pixel creeper_minigame_hitbox;
-		creeper_minigame_hitbox.x = 600;
-		creeper_minigame_hitbox.y = 200;
-
-		Pixel connect_hitbox;
-		connect_hitbox.x = 600;
-		connect_hitbox.y = 300;
-
-		graphics_draw_rectangle_filled(creeper_minigame_hitbox.x, creeper_minigame_hitbox.y, 100, 50, MAGENTA);
-		graphics_draw_rectangle_filled(connect_hitbox.x, connect_hitbox.y, 100, 50, TEAL);
-
-		if (touchscreen_is_touch_in_box(touch_location, creeper_minigame_hitbox, 100, 50)) {
-			return STATE_CREEPER_MINIGAME;
-		} else if (touchscreen_is_touch_in_box(touch_location, connect_hitbox, 100, 50)) {
-			if (data->journey_complete)
-				return STATE_REPORT_RESULTS;
-			else
-				return STATE_RECEIVE_COORDINATES;
-		}
-	}
-
-	return STATE_JOURNEY;
+	return main_controller_journey_menu_buttons(*data);
 }
 
 /**
@@ -210,4 +181,41 @@ static state_t main_controller_state_report_results(savedata_t *data) {
 
 	/* Ready for next adventure! */
 	return STATE_RECEIVE_COORDINATES;
+}
+
+/**
+ * This is just a janky helper function. Needs to be refactored later.
+ */
+static state_t main_controller_journey_menu_buttons(const savedata_t data) {
+	Pixel touch_location;
+	boolean touch_result;
+
+	static Pixel creeper_minigame_hitbox;
+	creeper_minigame_hitbox.x = 600;
+	creeper_minigame_hitbox.y = 200;
+
+	static Pixel connect_hitbox;
+	connect_hitbox.x = 600;
+	connect_hitbox.y = 300;
+
+	static int button_width = 100;
+	static int button_height = 50;
+
+	touch_result = touchscreen_get_press(&touch_location, 800);
+
+	if (touch_result) {
+		graphics_draw_rectangle_filled(creeper_minigame_hitbox.x, creeper_minigame_hitbox.y, button_width, button_height, MAGENTA);
+		graphics_draw_rectangle_filled(connect_hitbox.x, connect_hitbox.y, button_width, button_height, TEAL);
+
+		if (touchscreen_is_touch_in_box(touch_location, creeper_minigame_hitbox, button_width, button_height)) {
+			return STATE_CREEPER_MINIGAME;
+		} else if (touchscreen_is_touch_in_box(touch_location, connect_hitbox, button_width, button_height)) {
+			if (data.journey_complete)
+				return STATE_REPORT_RESULTS;
+			else
+				return STATE_RECEIVE_COORDINATES;
+		}
+	}
+
+	return STATE_JOURNEY;
 }
