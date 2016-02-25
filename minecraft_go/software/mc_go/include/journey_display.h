539deedb774df7424096c7d6811c7708107ded21
Derek Chan <derekchan1994@gmail.com>
2016-02-24 19:57:13 -0800

    Added missing headers to journey_display.h, fixed formatting in journey_display.c

--- minecraft_go/software/mc_go/include/journey_display.h
+++ minecraft_go/software/mc_go/include/journey_display.h
@@ -13,8 +13,13 @@
 void displayElapsedTime(Time *elapsedTime, int start_x_coord, int y_coord);
 void displayDestDistance(double dist_km,  int start_x_coord, int y_coord);
 void displaySpeed(float speed_km_h,  int start_x_coord, int y_coord);
-
-
-
+void displayKillCount(int creeper_kill, int start_x_coord, int y_coord);
+void displayMenu(int start_x_coord, int y_coord);
+void displayFight();
+void displaySmash();
+void displayWin();
+void displayLose();
+void displayTextBox(char* text, int text_color, int border_color, int box_color);
+void displayBackground();
 
 #endif /* JOURNEY_DISPLAY_H_ */
--- minecraft_go/software/mc_go/src/journey_display.c
+++ minecraft_go/software/mc_go/src/journey_display.c
@@ -13,178 +13,183 @@
 #include "gps.h"
 #include "Colours.h"
 
-
-
-void displayElapsedTime(Time *elapsedTime, int start_x_coord, int y_coord){
+void displayElapsedTime(Time *elapsedTime, int start_x_coord, int y_coord) {
 	char buffer[100];
 
-	sprintf(buffer, "Elapsed Time: %dhr %dmin %dsec", elapsedTime->hour, elapsedTime->minute, elapsedTime->second);
+	sprintf(buffer, "Elapsed Time: %dhr %dmin %dsec", elapsedTime->hour,
+			elapsedTime->minute, elapsedTime->second);
 
 	int i = 0;
 
-	while(buffer[i]){
+	while (buffer[i]) {
 		printf("%c", buffer[i]);
-		OutGraphicsCharFont4(start_x_coord + i*13, y_coord, RED, GRAPHICS_BACKGROUND_COLOUR , buffer[i], TRUE);
+		OutGraphicsCharFont4(start_x_coord + i * 13, y_coord, RED,
+				GRAPHICS_BACKGROUND_COLOUR, buffer[i], TRUE);
 		i++;
 	}
 }
 
-void displayDestDistance(double dist_km,  int start_x_coord, int y_coord){
+void displayDestDistance(double dist_km, int start_x_coord, int y_coord) {
 	char buffer[100];
 
 	sprintf(buffer, "Destination Distance: %.2fkm", dist_km);
 
 	int i = 0;
 
-	while(buffer[i]){
+	while (buffer[i]) {
 		printf("%c", buffer[i]);
-		OutGraphicsCharFont4(start_x_coord + i*13, y_coord, RED, GRAPHICS_BACKGROUND_COLOUR , buffer[i], TRUE);
+		OutGraphicsCharFont4(start_x_coord + i * 13, y_coord, RED,
+				GRAPHICS_BACKGROUND_COLOUR, buffer[i], TRUE);
 		i++;
 	}
 }
 
-void displaySpeed(float speed_km_h,  int start_x_coord, int y_coord){
+void displaySpeed(float speed_km_h, int start_x_coord, int y_coord) {
 	char buffer[100];
 
 	sprintf(buffer, "Current Speed: %.2fkm/h", speed_km_h);
 
 	int i = 0;
 
-	while(buffer[i]){
+	while (buffer[i]) {
 		printf("%c", buffer[i]);
-		OutGraphicsCharFont4(start_x_coord + i*13, y_coord, RED, GRAPHICS_BACKGROUND_COLOUR , buffer[i], TRUE);
+		OutGraphicsCharFont4(start_x_coord + i * 13, y_coord, RED,
+				GRAPHICS_BACKGROUND_COLOUR, buffer[i], TRUE);
 		i++;
 	}
 }
 
-void displayKillCount(int creeper_kill,  int start_x_coord, int y_coord){
+void displayKillCount(int creeper_kill, int start_x_coord, int y_coord) {
 	char buffer[100];
 
 	sprintf(buffer, "Creepers Killed: %d", creeper_kill);
 
 	int i = 0;
 
-	while(buffer[i]){
+	while (buffer[i]) {
 		printf("%c", buffer[i]);
-		OutGraphicsCharFont4(start_x_coord + i*13, y_coord, RED, GRAPHICS_BACKGROUND_COLOUR , buffer[i], TRUE);
+		OutGraphicsCharFont4(start_x_coord + i * 13, y_coord, RED,
+				GRAPHICS_BACKGROUND_COLOUR, buffer[i], TRUE);
 		i++;
 	}
 }
 
-void displayMenu(  int start_x_coord, int y_coord){
+void displayMenu(int start_x_coord, int y_coord) {
 	char buffer[100];
 
 	sprintf(buffer, "Minecraft GO");
 
-
-
 	int i = 0;
 
-	while(buffer[i]){
+	while (buffer[i]) {
 		printf("%c", buffer[i]);
-		OutGraphicsCharFont5(start_x_coord + i*32, y_coord, WHITE, GRAPHICS_BACKGROUND_COLOUR , buffer[i], TRUE);
+		OutGraphicsCharFont5(start_x_coord + i * 32, y_coord, WHITE,
+				GRAPHICS_BACKGROUND_COLOUR, buffer[i], TRUE);
 		i++;
 	}
-	HLine(start_x_coord, y_coord+55, start_x_coord+9*32, WHITE);
+	HLine(start_x_coord, y_coord + 55, start_x_coord + 9 * 32, WHITE);
 }
 
-void displayFight(){
+void displayFight() {
 	char buffer[100];
 
-	graphics_draw_rectangle_filled(225, 150, 11*32, 200, WHITE);
-	graphics_draw_rectangle_border(225, 150, 11*32, 200, BLACK);
+	graphics_draw_rectangle_filled(225, 150, 11 * 32, 200, WHITE);
+	graphics_draw_rectangle_border(225, 150, 11 * 32, 200, BLACK);
 
 	sprintf(buffer, "FIGHT!!!!");
 
 	int i = 0;
 
-	while(buffer[i]){
+	while (buffer[i]) {
 		printf("%c", buffer[i]);
-		OutGraphicsCharFont5(250 + i*32, 225, GREEN, GRAPHICS_BACKGROUND_COLOUR , buffer[i], FALSE);
+		OutGraphicsCharFont5(250 + i * 32, 225, GREEN,
+				GRAPHICS_BACKGROUND_COLOUR, buffer[i], FALSE);
 		i++;
 	}
 }
 
-void displaySmash(){
+void displaySmash() {
 	char buffer[100];
 
-	graphics_draw_rectangle_filled(225, 150, 11*32, 200, WHITE);
-	graphics_draw_rectangle_border(225, 150, 11*32, 200, BLACK);
+	graphics_draw_rectangle_filled(225, 150, 11 * 32, 200, WHITE);
+	graphics_draw_rectangle_border(225, 150, 11 * 32, 200, BLACK);
 
 	sprintf(buffer, "SMASH!!!!");
 
 	int i = 0;
 
-	while(buffer[i]){
+	while (buffer[i]) {
 		printf("%c", buffer[i]);
-		OutGraphicsCharFont5(250 + i*32, 225, GREEN, GRAPHICS_BACKGROUND_COLOUR , buffer[i], TRUE);
+		OutGraphicsCharFont5(250 + i * 32, 225, GREEN,
+				GRAPHICS_BACKGROUND_COLOUR, buffer[i], TRUE);
 		i++;
 	}
 }
 
-void displayWin(){
+void displayWin() {
 	char buffer[100];
 
-	graphics_draw_rectangle_filled(225, 150, 11*32, 200, WHITE);
-	graphics_draw_rectangle_border(225, 150, 11*32, 200, BLACK);
+	graphics_draw_rectangle_filled(225, 150, 11 * 32, 200, WHITE);
+	graphics_draw_rectangle_border(225, 150, 11 * 32, 200, BLACK);
 
 	sprintf(buffer, "YOU WIN!!!");
 
 	int i = 0;
 
-	while(buffer[i]){
+	while (buffer[i]) {
 		printf("%c", buffer[i]);
-		OutGraphicsCharFont5(250 + i*32, 225, GREEN, GRAPHICS_BACKGROUND_COLOUR , buffer[i], TRUE);
+		OutGraphicsCharFont5(250 + i * 32, 225, GREEN,
+				GRAPHICS_BACKGROUND_COLOUR, buffer[i], TRUE);
 		i++;
 	}
 }
 
-void displayLose(){
+void displayLose() {
 	char buffer[100];
 
-	graphics_draw_rectangle_filled(225, 150, 11*32, 200, WHITE);
-	graphics_draw_rectangle_border(225, 150, 11*32, 200, BLACK);
+	graphics_draw_rectangle_filled(225, 150, 11 * 32, 200, WHITE);
+	graphics_draw_rectangle_border(225, 150, 11 * 32, 200, BLACK);
 
 	sprintf(buffer, "GAME OVER");
 
 	int i = 0;
 
-	while(buffer[i]){
+	while (buffer[i]) {
 		printf("%c", buffer[i]);
-		OutGraphicsCharFont5(250 + i*32, 225, RED, GRAPHICS_BACKGROUND_COLOUR , buffer[i], TRUE);
+		OutGraphicsCharFont5(250 + i * 32, 225, RED, GRAPHICS_BACKGROUND_COLOUR,
+				buffer[i], TRUE);
 		i++;
 	}
 }
 
-void displayTextBox(char* text, int text_color, int border_color, int box_color){
+void displayTextBox(char* text, int text_color, int border_color, int box_color) {
 	char buffer[100];
 
-	graphics_draw_rectangle_filled(225, 150, 12*32, 200, box_color);
-	graphics_draw_rectangle_border(225, 150, 12*32, 200, border_color);
+	graphics_draw_rectangle_filled(225, 150, 12 * 32, 200, box_color);
+	graphics_draw_rectangle_border(225, 150, 12 * 32, 200, border_color);
 
 	sprintf(buffer, text);
 
 	int i = 0;
 
-	while(buffer[i]){
+	while (buffer[i]) {
 		printf("%c", buffer[i]);
-		OutGraphicsCharFont5(250 + i*32, 225, text_color, GRAPHICS_BACKGROUND_COLOUR , buffer[i], TRUE);
+		OutGraphicsCharFont5(250 + i * 32, 225, text_color,
+				GRAPHICS_BACKGROUND_COLOUR, buffer[i], TRUE);
 		i++;
 	}
 }
 
-void displayBackground(){
+void displayBackground() {
 
 	int i;
-	for(i = 0; i < 3; i++){
-		soil_block_generator(i*240, 240);
-		grass_block_generator(i*240,0);
+	for (i = 0; i < 3; i++) {
+		soil_block_generator(i * 240, 240);
+		grass_block_generator(i * 240, 0);
 	}
 
 	soil_block_generator(560, 240);
-	grass_block_generator(560,0);
-
+	grass_block_generator(560, 0);
 
 }
-
 
