d846facebc0f9f15b1f8066602414f408ec5aaa9
loganfg <logan.gilmore@alumni.ubc.ca>
2016-03-03 17:25:43 -0800

    Edited names of font functions

--- minecraft_go/software/mc_go/include/font.h
+++ minecraft_go/software/mc_go/include/font.h
@@ -8,10 +8,10 @@
 #ifndef FONT_H_
 #define FONT_H_
 
-void graphics_char_font_1(int x, int y, int fontcolour, int backgroundcolour, int c, int Erase);
-void graphics_char_font_2(int x, int y, int fontcolour, int backgroundcolour, int c, int Erase);
-void graphics_char_font_3(int x, int y, int fontcolour, int backgroundcolour, int c, int Erase);
-void graphics_char_font_4(int x, int y, int fontcolour, int backgroundcolour, int c, int Erase);
-void graphics_char_font_5(int x, int y, int fontcolour, int backgroundcolour, int c, int Erase);
+void fonts_char_1(int x, int y, int fontcolour, int backgroundcolour, int c, int Erase);
+void fonts_char_2(int x, int y, int fontcolour, int backgroundcolour, int c, int Erase);
+void fonts_char_3(int x, int y, int fontcolour, int backgroundcolour, int c, int Erase);
+void fonts_char_4(int x, int y, int fontcolour, int backgroundcolour, int c, int Erase);
+void fonts_char_5(int x, int y, int fontcolour, int backgroundcolour, int c, int Erase);
 
 #endif /* FONT_H_ */
